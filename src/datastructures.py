8b94a545f85e35a493bb6bf67e99e3a31cdb6430
Juan L <lauredojuan@gmail.com>
2020-08-20 21:23:27 +0000

    first commit

--- src/datastructures.py
+++ src/datastructures.py
@@ -13,7 +13,10 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members =[{ "id": 0,"first_name": "John" ,  "last_name": last_name , "age": 33, "Lucky Numbers": [7, 13, 22] },
+                         { "id": 1,"first_name": "Jane" ,  "last_name": last_name , "age": 35, "Lucky Numbers": [10, 14, 3] },
+                         { "id": 2,"first_name": "Jimmy" ,  "last_name": last_name, "age": 5, "Lucky Numbers": [1] }] 
+
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
@@ -24,8 +27,10 @@ class FamilyStructure:
         # fill this method and update the return
         return None
 
+        
     def delete_member(self, id):
         # fill this method and update the return
+
         for position in range(len(self._members)):
             if self._members[position]["id"] == id:
                 self._members.pop(position)
